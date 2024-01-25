fd880714f4bc71a781886aaccf765977306c89d4
Alexandre TL <alextorresleguet@icloud.com>
2024-01-25 09:54:17 +0100

    wandb tracking

--- train.py
+++ train.py
@@ -1,10 +1,18 @@
+"""
+main training script.
+
+todo : 
+-gradient acc ?
+
+"""
+
 import os
 import math
 
 import torch
 import torch.nn.functional as F
 
-import matplotlib.pyplot as plt
+import wandb
 
 from models.lm import LM
 from models.transformer.transformer import TransformerConfig
@@ -20,14 +28,14 @@ d_model = 512
 n_layers = 8
 n_heads = 8
 
-B = 1
+batch_size = 1
 
-num_iters = 500
+num_iters = 500 # todo : up iters and lr_decays_iters
 
 lr = 5e-4 # todo : up BS and LR
 lr_min = 1e-5
-lr_warmup_iter = 100
-lr_decay_iter = 10000 # max_iters as in chinchilla
+lr_warmup_iters = 100
+lr_decay_iters = 10000 # max_iters as in chinchilla
 
 dropout = 0.
 bias = False
@@ -45,19 +53,39 @@ data_dir = "data/"
 
 # -------------------------------------------------------
 
+wandb.init(project="othello",
+           config={
+               "architecture": "Transformer",
+               "d_model": d_model,
+               "n_layers": n_layers,
+               "n_heads": n_heads,
+               "num_iters": num_iters,
+               "batch_size": batch_size,
+               "lr": lr,
+               "lr_min": lr_min,
+               "lr_warmup_iters": lr_warmup_iters,
+               "lr_decay_iters": lr_decay_iters,
+               "dropout": dropout,
+               "bias": bias,
+               "adam_b1": adam_b1,
+               "adam_b2": adam_b2,
+               "clip_value_grad": clip_value_grad,
+               "weight_decay": weight_decay,
+           })
+
 # cosine with warmup (taken from @karpathy)
 def get_lr(it):
-    if lr_decay_iter == 0:
+    if lr_decay_iters == 0:
         return lr
     
     # 1) linear warmup for warmup_iters steps
-    if it < lr_warmup_iter:
-        return lr * it / lr_warmup_iter
+    if it < lr_warmup_iters:
+        return lr * it / lr_warmup_iters
     # 2) if it > lr_decay_iters, return min learning rate
-    if it > lr_decay_iter:
+    if it > lr_decay_iters:
         return lr_min
     # 3) in between, use cosine decay down to min learning rate
-    decay_ratio = (it - lr_warmup_iter) / (lr_decay_iter - lr_warmup_iter)
+    decay_ratio = (it - lr_warmup_iters) / (lr_decay_iters - lr_warmup_iters)
     assert 0 <= decay_ratio <= 1
     coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))  # coeff ranges 0..1
     return lr_min + coeff * (lr - lr_min)
@@ -66,7 +94,7 @@ train_dir = os.path.join(data_dir, "train")
 val_dir = os.path.join(data_dir, "val")
 
 ds = OthelloDataset(train_dir)
-loader = torch.utils.data.DataLoader(ds, batch_size=B, num_workers=0, pin_memory=True)
+loader = torch.utils.data.DataLoader(ds, batch_size=batch_size, num_workers=0, pin_memory=True)
 
 ds_val = OthelloDataset(val_dir)
 loader_val = torch.utils.data.DataLoader(ds_val, batch_size=1, num_workers=0, pin_memory=True) # todo : bs de 1 ici, quand on l'augmentera attention a eval.py
@@ -84,7 +112,6 @@ if load_checkpoint:
     optim.load_state_dict(checkpoint['optimizer'])
     scaler.load_state_dict(checkpoint['scaler'])
 
-losses = []
 
 for iter, data in enumerate(loader):
     x, y = data
@@ -110,13 +137,15 @@ for iter, data in enumerate(loader):
     for param_group in optim.param_groups:
         param_group['lr'] = lr_iter
 
-    losses.append(loss.item())
-
     if iter >= num_iters:
         break
 
-plt.plot(losses)
-plt.savefig(run_name + ".png", dpi=600)
+checkpoint = {"model": model.state_dict(),
+              "optimizer": optim.state_dict(),
+              "scaler": scaler.state_dict()}
+torch.save(checkpoint, checkpoint_dir)
 
 from eval import eval
 print(eval(model, 50, loader_val))
+
+wandb.finish()
